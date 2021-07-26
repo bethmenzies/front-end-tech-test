8b86d9aa4d2bb9d34a8dfafb0d8ae0ffb53936d2
bethmenzies <bethmenzies@btinternet.com>
2021-07-26 17:51:18 +0100

    I have now realised that there are no darts events
    I'm committing this just so you can see what I've done. I am going to turn this test into one that checks there are no events and no GET request, and I'll test an event by using Football instead (as there's always football on!)

--- cypress/integration/Darts.feature
+++ cypress/integration/Darts.feature
@@ -2,12 +2,12 @@ Feature: Viewing a Darts event
 I want to navigate PokerStars to a Darts event
 
 Scenario: Navigating to the Darts sport page
-Given I am on the home page
+Given I am on the 'home' page
 When I select the Darts sports page
 Then I verify that I am on the Darts sports page
 
-#Scenario: Navigating to a Darts event
-#Given I am on the Darts sports page
-#When I select a Darts event
-#Then I verify that I am on the Darts event page
-#And I verify that an events GET request has been made
+Scenario: Navigating to a Darts event
+Given I am on the 'darts' page
+When I select a Darts event
+Then I verify that I am on the Darts event page
+And I verify that an events GET request has been made
--- cypress/integration/Darts/darts.js
+++ cypress/integration/Darts/darts.js
@@ -1,8 +1,11 @@
-import { Given , Then , When} from "cypress-cucumber-preprocessor/steps"
+import { And, Given , Then , When} from "cypress-cucumber-preprocessor/steps"
 
 // test steps
-Given('I am on the home page', () => {
-    cy.visit('/')
+Given('I am on the {string} page', (page) => {
+    if (page === 'home') {
+        page = ''
+    }
+    cy.visit(`/#/${page}`)
 })
 
 When('I select the Darts sports page', () => {
@@ -10,6 +13,20 @@ When('I select the Darts sports page', () => {
     cy.get('#extendedMenu__link__darts').click()
 })
 
+When('I select a Darts event', () => {
+    // it looks like there aren't any darts events
+    //so just going to verify that the outrights tab is selected
+    cy.get('#link__outrights').should('have.class', 'active')
+})
+
 Then('I verify that I am on the Darts sports page', () => {
     cy.location('hash').should('eq', '#/darts/outrights')
+})
+
+Then('I verify that I am on the Darts event page', () => {
+    cy.location('hash').should('eq', '#/darts/outrights')
+})
+
+And('I verify that an events GET request has been made', () => {
+    
 })
